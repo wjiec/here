4b92ee6821d4d1b3dc5d1b6ef56d453031ecfe6b
JShadowMan <jshadowman@163.com>
2015-12-02 16:01:00 +0800

    add route p1

--- include/Core/Here/Route.php
+++ include/Core/Here/Route.php
@@ -1,12 +1,31 @@
 <?php
 class Route {
-    private function match() {
+    private $_hook;
+    private $_error;
+    private $_tree;
+    
+    const SEPARATOR = '/';
+    
+    private function createNode() {
     }
     public function __call($name, $args) {
-        if (in_array($name, array('get', 'post'))) {
-        } else if (in_array($name, array('hook', 'error'))) {
+        if (in_array($name, array('get', 'post'))) { // 路径匹配, 路由
+            array_unshift($args, strtoupper($name));
+            echo '';
         }
-    }
-    public function run() {
+        if (in_array($name, array('error', 'hook'))) { // 钩子和错误处理
+            $key = array_shift($args);
+            $attr = '_' . $name;
+            
+            if (($attr = '_'. $name) && isset($args[0]) && is_callable($args[0])) {
+                $this->{$attr}[$key] = $args[0];
+            } else if (isset($this->{$attr}[$key]) && is_callable($this->{$attr}[$key])) {
+                return call_user_func_array($this->{$attr}[$key], $args);
+            } else {
+                return ($name == 'error') ? trigger_error("\"{$key}\" not defined to handler error: {$args[0]}") : $args[0];
+            }
+        }
+        
+        return $this;
     }
 }
--- index.php
+++ index.php
@@ -5,10 +5,52 @@
  * @package Here
  * @author  ShadowMan
  */
-
-var_dump($_SERVER);
 # TODO Route
+############################################################
+error_reporting(E_ALL);
+
+# Root Directory
+define('__HERE_ROOT_DIRECTORY__', dirname(__FILE__));
+
+# Common Resource Directory
+define('__HERE_COMMON_DIRECTORY__', '/include');
+
+# Admin Resource Directory
+define('__HERE_ADMIN_DIRECTORY__', '/admin');
+
+# Core Resource Directory
+define('__HERE_CORE_DIRECTORY__', '/include/Core');
+
+@set_include_path(get_include_path() . PATH_SEPARATOR.
+    __HERE_ROOT_DIRECTORY__ . __HERE_COMMON_DIRECTORY__ . PATH_SEPARATOR.
+    __HERE_ROOT_DIRECTORY__ . __HERE_ADMIN_DIRECTORY__ . PATH_SEPARATOR.
+    __HERE_ROOT_DIRECTORY__ . __HERE_CORE_DIRECTORY__ . PATH_SEPARATOR
+);
+ob_start();
+
+# Route API
+require_once 'Here/Route.php';
+
+class Handle {
+    public function handle_401() {
+        echo '401';
+    }
+}
+
+(new Route())
+->error('401', array('Handle', 'handle_401'))
+->error('403', function() {
+    echo '403';
+})
+->error('404', function() {
+    echo '404';
+})
+->get('/', function($params) {
+    echo 'Hello';
+});
+
 die(); # Route Debug
+
 
 if (!defined('__HERE_ROOT_DIRECTORY__') && !@include_once 'config.php') {
     file_exists('./install.php') ? header('Location: install.php') : print('Missing Config File');
