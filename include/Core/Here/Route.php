d0b8d149b792932abd430e3fb3af6225507385fa
JShadowMan <jshadowman@163.com>
2015-12-03 22:39:03 +0800

    Simple Route Done.

--- include/Core/Here/Route.php
+++ include/Core/Here/Route.php
@@ -3,29 +3,115 @@ class Route {
     private $_hook;
     private $_error;
     private $_tree;
-    
+
     const SEPARATOR = '/';
+    const HANDLE = '$$';
+
+    const CALLBACK = 'cb';
+    const HOOK = 'hook';
     
-    private function createNode() {
+    public function __construct() {
+        $this->_error = [];
+        $this->_hook  = [];
+        $this->_tree  = [];
     }
+
     public function __call($name, $args) {
-        if (in_array($name, array('get', 'post'))) { // 路径匹配, 路由
+        if (in_array($name, array('get', 'post'))) {
             array_unshift($args, strtoupper($name));
-            echo '';
+            call_user_func_array('self::createNode', $args);
         }
         if (in_array($name, array('error', 'hook'))) { // 钩子和错误处理
-            $key = array_shift($args);
-            $attr = '_' . $name;
-            
-            if (($attr = '_'. $name) && isset($args[0]) && is_callable($args[0])) {
-                $this->{$attr}[$key] = $args[0];
-            } else if (isset($this->{$attr}[$key]) && is_callable($this->{$attr}[$key])) {
-                return call_user_func_array($this->{$attr}[$key], $args);
+            $key  = array_shift($args); // _error | _hook 中的键, 表示一个错误或者一个钩子，值为需要触发的方法
+            $member = '_'. $name; // _error($this->_error) & _hook($this->_hook)
+    
+            if (isset($args[0]) && is_callable($args[0])) { // $argv[0] -> callback, 回调方法
+                $this->{$member}[$key] = $args[0]; // 设置回调
+            } else if (isset($this->{$member}[$key]) && is_callable($this->{$member}[$key])) { // 如果只传入 key(和不是一个可调用结构的参数), 检查这个键是否存在, 存在就调用这个回调函数
+                return call_user_func_array($this->{$member}[$key], $args);
+            } else { // 调用错误, 即不是设置对应 error | hook 的回调函数，也不是触发相应 error | hook 的
+                return ($name == 'error') ? trigger_error("`{$name}`->\"{$key}\": {$args[0]}") : $args[0]; // set_error_handler($error_handler);
+            }
+        }
+
+        return $this;
+    }
+    
+    public function execute($method = null, $path = null, $params = []) {
+        $method = $method ? $method : $_SERVER['REQUEST_METHOD'];
+        $path = trim($path ? $path : parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), self::SEPARATOR);
+        
+        list($callback, $hook, $params) = self::resolve($method, $path, $params);
+        if (!is_callable($callback)) {
+            if (empty($params[0])) {
+                echo $params[0];
             } else {
-                return ($name == 'error') ? trigger_error("\"{$key}\" not defined to handler error: {$args[0]}") : $args[0];
+                $this->error('404');
+            }
+        } else {
+            call_user_func_array($callback, $params);
+        }
+    }
+    
+    private function createNode($method, $path, $callback, $hook = null) {
+        $nodes = explode(self::SEPARATOR, str_replace('.', self::SEPARATOR, trim($path, self::SEPARATOR)));
+        if (!is_array($method)) {
+            $method = [ $method ];
+        }
+        foreach($method as $m){
+            if (!array_key_exists($m, $this->_tree)) { // 如果在 _tree 中， $method 键值对不存在，就创建这个键值对。值为array
+                $this->_tree[$m] = [];
+            }
+            $this->match_one_path($this->_tree[$m], $nodes, $callback, $hook);
+        }
+    }
+
+    private function match_one_path(&$tree, $nodes, $callback, $hook) {
+        $currentNode = array_shift($nodes);
+        if ($currentNode && !array_key_exists($currentNode, $tree)) {
+            $tree[$currentNode] = [];
+        }
+        
+        if ($currentNode) { // create next node
+            return self::match_one_path($tree[$currentNode], $nodes, $callback, $hook);
+        }
+
+        $tree[self::HANDLE] = [ self::CALLBACK => $callback, self::HOOK => [] ];
+        if (is_array($hook)) {
+            foreach ($hook as $h) {
+                array_push($tree[self::HANDLE]['hook'], $h);
+            }
+        } else if ($hook) {
+            array_push($tree[self::HANDLE]['hook'], $hook);
+        }
+    }
+    
+    private function resolve($method, $path, $params) {
+        if (!array_key_exists($method, $this->_tree)) {
+            return [null, null, ["METHOD \"{$method}\" NOT FOUND"]];
+        }
+        $nodes = explode(self::SEPARATOR, str_replace('.', self::SEPARATOR, $path));
+        
+        return self::__find($this->_tree[$method], $nodes, $params);
+    }
+    
+    private function __find(&$tree, $nodes, $params) {
+        $need = array_shift($nodes);
+        
+        if (empty($need)) {
+            if (array_key_exists(self::HANDLE, $tree)) {
+                return [$tree[self::HANDLE][self::CALLBACK], $tree[self::HANDLE][self::HOOK], $params];
+            } else {
+                return [null, null, ["\"HANDLE NOT FOUND\""]];
+            }
+        }
+        foreach ($tree as $node => $value) {
+            if ($node == self::HANDLE) { continue; }
+            if ($node == $need) {
+                return $this->__find($tree[$node], $nodes, $params);
             }
         }
         
-        return $this;
+        return [null, null, ["PATH NOT FOUND"]];
     }
 }
--- index.php
+++ index.php
@@ -21,33 +21,60 @@ define('__HERE_ADMIN_DIRECTORY__', '/admin');
 # Core Resource Directory
 define('__HERE_CORE_DIRECTORY__', '/include/Core');
 
+# Plugins Directory
+define('__HERE_PLUGINS_DIRECTORY__', '/include/Plugins');
+
+# Theme Directory
+define('__HERE_THEME_DIRECTORY__', '/include/Theme');
+
 @set_include_path(get_include_path() . PATH_SEPARATOR.
     __HERE_ROOT_DIRECTORY__ . __HERE_COMMON_DIRECTORY__ . PATH_SEPARATOR.
     __HERE_ROOT_DIRECTORY__ . __HERE_ADMIN_DIRECTORY__ . PATH_SEPARATOR.
-    __HERE_ROOT_DIRECTORY__ . __HERE_CORE_DIRECTORY__ . PATH_SEPARATOR
+    __HERE_ROOT_DIRECTORY__ . __HERE_CORE_DIRECTORY__ . PATH_SEPARATOR.
+    __HERE_ROOT_DIRECTORY__ . __HERE_PLUGINS_DIRECTORY__ . PATH_SEPARATOR.
+    __HERE_ROOT_DIRECTORY__ . __HERE_THEME_DIRECTORY__ . PATH_SEPARATOR
 );
 ob_start();
 
 # Route API
 require_once 'Here/Route.php';
 
-class Handle {
-    public function handle_401() {
-        echo '401';
+// DEBUG:
+//      php -S 0.0.0.0:9527
+/*
+(new Route())
+->error('404', function($params) {
+    echo '404' . ' ' . $params;
+})
+->error('404', 'Not Found'); // curl -vvv 127.0.0.1:9527 -> '404 Not Found'
+*/
+class Handler{
+    public function hello($name){
+        echo "Hello $name !!!";
+    }
+    public static function hello_again($name){
+        echo "Hello $name again !!!";
     }
 }
 
 (new Route())
-->error('401', array('Handle', 'handle_401'))
-->error('403', function() {
-    echo '403';
-})
 ->error('404', function() {
-    echo '404';
+    header("HTTP/1.1 404 Not Found");
+    include 'default/404.php';
 })
-->get('/', function($params) {
-    echo 'Hello';
-});
+->hook('print', function() {
+    echo '<br>HOOK: print<br>';
+})
+->get('/', function() {
+    echo '<br>GET /<br>';
+})
+->get('/hello/world/', function() {
+    echo '<br>GET /hell/world/<br>';
+})
+->get('/license.php', function() {
+    include 'default/license.php';
+})
+->execute();
 
 die(); # Route Debug
 
