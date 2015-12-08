090ce8a6bc0f1cece4c668dd1c1a187637c7be6f
JShadowMan <jshadowman@163.com>
2015-12-08 20:28:52 +0800

    add Class.Controller and controller.installer

--- include/Core/Controller/installer.php
+++ include/Core/Controller/installer.php
@@ -1,1 +1,14 @@
 <?php
+
+class Controller_Installer {
+    public static function step() {
+        if (!isset($_POST['step'])) {
+            exit;
+        }
+        switch ($_POST['step']) {
+            case '2': echo '2'; break;
+            case '3': echo '3'; break;
+            default: exit();
+        }
+    }
+}
---
+++ include/Core/Here/Controller.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author ShadowMan
+ * @package Controller
+ */
+class Controller {
+    const _path = 'include/Core/Controller/';
+    const _pref = 'Controller_';
+    const _PHP = '.php';
+    
+    public static function request($controller, $action, $params = null) {
+        if (file_exists(self::_path . $controller . self::_PHP) && !is_dir(self::_path . $controller . self::_PHP)) {
+            if (!class_exists(self::_pref . ucfirst($controller))) {
+                throw new Exception("CLASS NOT FOUND");
+            }
+            if (!is_callable([self::_pref . ucfirst($controller), $action])) {
+                throw new Exception("METHOD NOT FOUND");
+            }
+            call_user_func(self::_pref . ucfirst($controller) . "::" . $action);
+        } else {
+            throw new Exception("CONTROLLER NOT FOUND");
+        }
+    }
+}
+
+?>
