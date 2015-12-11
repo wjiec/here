70ea00b5c477a305b7a05694fdf896ff97cd7ef9
JShadowMan <jshadowman@163.com>
2015-12-11 14:44:19 +0800

    change dom

--- admin/install/step/2.php
+++ admin/install/step/2.php
@@ -2,41 +2,37 @@
   <h3>Here Setting</h3>
   <form action="/install.php" method="POST">
     <div class="widget-form-group">
+      <!-- Database Host -->
       <div class="widget-input-group">
-        <label class="widget-input-label" for="db-addr">DB ADDR</label>
+        <label class="widget-input-label" for="db-addr"><em>DB Host</em></label>
         <input type="text" id="db-addr" class="widget-form-control" name="db-addr" value="localhost" required="required"/>
       </div>
       <p class="_Here-Form-Tips">You should be able to get this info from your web host, if localhost does not work.</p>
-    </div>
-    <div class="widget-form-group">
+      <!-- Database Port -->
       <div class="widget-input-group">
         <label class="widget-input-label" for="db-port">DB PORT</label>
         <input type="text" id="db-port" class="widget-form-control" name="db-port" value="3306" required="required"/>
       </div>
       <p class="_Here-Form-Tips">MySQL Server port</p>
-    </div>
-    <div class="widget-form-group">
+      <!-- Database Username -->
       <div class="widget-input-group">
         <label class="widget-input-label" for="db-user">DB USER</label>
         <input type="text" id="db-user" class="widget-form-control" name="db-user" value="root" required="required"/>
       </div>
       <p class="_Here-Form-Tips">Your MySQL username.</p>
-    </div>
-    <div class="widget-form-group">
+      <!-- Database Password -->
       <div class="widget-input-group">
         <label class="widget-input-label" for="db-pawd">DB PAWD</label>
         <input type="password" id="db-pawd" class="widget-form-control" name="db-pawd" placeholder="enter password" autofocus="autofocus" required="required"/>
       </div>
       <p class="_Here-Form-Tips">and your MySQL password.</p>
-    </div>
-    <div class="widget-form-group">
+      <!-- Database Name -->
       <div class="widget-input-group">
         <label class="widget-input-label" for="db-name">DB NAME</label>
         <input type="text" id="db-name" class="widget-form-control" name="db-name" value="here" required="required"/>
       </div>
       <p class="_Here-Form-Tips">The name of the database you want to run WP in.</p>
-    </div>
-    <div class="widget-form-group">
+      <!-- Database Pref -->
       <div class="widget-input-group">
         <label class="widget-input-label" for="db-pref">DB PREF</label>
         <input type="text" id="db-pref" class="widget-form-control" name="db-pref" value="h_" required="required"/>
