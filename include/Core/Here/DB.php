19ef0d3e402757c5d8b46857bda491ea35e4ca96
JShadowMan <jshadowman@163.com>
2015-12-17 22:58:29 +0800

    update Core.DB

--- include/Core/Here/DB.php
+++ include/Core/Here/DB.php
@@ -1,18 +1,19 @@
 <?php
 /**
  * @author ShadowMan
- * @package DB
+ * @package Core.DB
  */
 class DB {
     public function __construct() {
     }
-    
+
     public static function connectTest($user, $password, $database, $host, $port = 3306) {
         $conn = @new mysqli($host, $user, $password, $database, $port);
-        
+
         if ($conn->connect_errno) {
             throw new Exception($conn->connect_error, $conn->connect_errno);
         }
+        return $conn->close();
     }
 }
 
