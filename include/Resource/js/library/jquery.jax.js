5495d8af180b5476288a1be5b42527b02bc85241
JShadowMan <jshadowman@163.com>
2016-01-25 16:51:28 +0800

    jax basically complete and wait onpopstate

--- include/Resource/js/library/jquery.jax.js
+++ include/Resource/js/library/jquery.jax.js
@@ -11,6 +11,7 @@ $.support.pjax = window.history && window.history.pushState && window.history.re
 $.support.storage = !!window.localStorage
 
 var stack = []
+var globalState = null
 var lastXHR = null
 var defaultOptions = {
     timeout: 650,
@@ -19,7 +20,8 @@ var defaultOptions = {
     type: 'GET',
     dataType: 'HTML',
     scrollTo: null,
-    fullReplace: false
+    fullReplace: false,
+    urlReplace: true
 }
 
 function d(msg) {
@@ -80,6 +82,46 @@ function stripHash(location) {
     return location.href.replace(/#.*/, '')
 }
 
+function parseHTML(html) {
+    return $.parseHTML(html, document, true)
+}
+
+function filter(el, selector) {
+    return el.filter(selector).add(el.find(selector))
+}
+
+function extractContainer(data, xhr, options) {
+    var obj = {}
+    var fullDocument = /<html/i.test(data)
+
+    obj.url = options.url
+    if (fullDocument) {
+        var head = $(parseHTML(data.match(/<head[^>]*>([\s\S]*)<\/head>/i)[0]))
+        var body = $(parseHTML(data.match(/<body[^>]*>([\s\S]*)<\/body>/i)[0]))
+    } else {
+        var head, body
+        head = body = $(parseHTML(data))
+    }
+    
+    if (body.length == 0) {
+        return obj
+    }
+
+    var selector = options.container.selector
+    obj.contents = filter(body, selector).first().text()
+    if (options.fullReplace) {
+        obj.contents = data
+    }
+
+    obj.title = $.trim(filter(head, 'title').last().text())
+
+    return obj
+}
+
+function unixStamp() {
+    return int((new Date).getTime())
+}
+
 function entry(selector, container, options) {
     var context = this
 
@@ -96,10 +138,17 @@ function handleClick(event, container, options) {
     options = optionsFor(container, options)
 
     var el = event.currentTarget
-    if (el.tagName.toUpperCase() !== 'A') {
+    if (el.tagName.toUpperCase() !== 'A' && el.tagName.toUpperCase() !== 'BUTTON') {
         throw 'require an anchor element'
     }
 
+    if (el.tagName.toUpperCase() === 'BUTTON') { // Replace el
+        el = document.createElement('a')
+
+        if (options.url) {
+            el.href = options.url
+        }
+    }
     if (event.which > 1 || event.ctrlKey || event.altKey || event.shiftKey) {
         return
     }
@@ -130,7 +179,7 @@ function handleClick(event, container, options) {
 
 function jax(options) {
     options = $.extend(true, {}, $.ajaxSettings, defaultOptions, options)
-    
+
     if ($.isFunction(options.url)) {
         options.url = options.url()
     }
@@ -150,6 +199,9 @@ function jax(options) {
     }
 
     var timeoutTimer = null
+    /**
+     * brforeSend Member
+     */
     options.beforeSend = function(xhr, settings) {
         if (settings.type.toUpperCase() !== 'GET') {
             settings.timeout = 0
@@ -171,20 +223,63 @@ function jax(options) {
             settings.timeout = 0
         }
     }
-
+    /**
+     * Complete 
+     */
     options.complete = function(xhr, textStatus) {
         if (timeoutTimer) {
             clearTimeout(timeoutTimer)
         }
         trigger('jax:complete', [xhr, textStatus, options])
     }
-
+    /**
+     * Error Handle
+     */
     options.error = function(xhr, textStatus, errorThrown) {
+        var allowed = trigger('jax:error', [xhr, textStatus, errorThrown, options])
+        if (options.type.toUpperCase() == 'GET' && textStatus !== 'abort' && allowed) {
+            var contents = extractContainer("", xhr, options)
+            // hard reload ?
+        }
+    }
+    /**
+     * Success
+     */
+    options.success = function(data, status, xhr) {
+        var responsed = extractContainer(data, xhr, options)
+        var prevState = globalState
+
+        globalState = {
+            id: unixStamp(),
+            url: responsed.url,
+            title: responsed.title,
+            container: options.container.selector,
+            timeout: options.timeout,
+            full: {
+                document: /<html/i.test(data),
+                replace: options.fullReplace,
+                base: location.href
+            }
+        }
+
+        window.history.replaceState(globalState, responsed.title, options.urlReplace ? responsed.url : null)
+        if ($.contains(options.container, document.activeElement)) {
+            try {
+                document.activeElement.blur()
+            } catch (e) {}
+        }
+        
+        if (responsed.title) {
+            document.title = responsed.title
+        }
+        
+        trigger('jax:beforeReplace', [responsed.contents, options], {
+            state: globalState,
+            prevState: prevState
+        })
+        context.html(responsed.contents)
     }
 
-    options.success = function(data, status, xhr) {
-    }
-    
     lastXHR = $.ajax(options)
 }
 
