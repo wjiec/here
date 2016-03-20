465ab12a05753b29b008e825d22ab8a7510475c6
JShadowMan <jshadowman@163.com>
2016-03-20 20:30:04 +0800

    <refactor>(script): table structure complete

--- admin/install/scripts/mysql.sql
+++ admin/install/scripts/mysql.sql
@@ -1,10 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.1.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: 2015-12-25 22:06:58
-
 --
 -- Database: `{%database%}`
 --
@@ -12,22 +5,94 @@
 -- --------------------------------------------------------
 
 --
--- 表的结构 `here_users`
+-- Table structure for table `here_users`
 --
 
 CREATE TABLE IF NOT EXISTS `here_users` (
-  `uid` smallint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `url` varchar(64) NOT NULL,
-  `status` smallint(8) DEFAULT NULL,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `name` (`uid`,`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `uid` SMALLINT(8) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(32) NOT NULL,
+  `password` VARCHAR(64) NOT NULL,
+  `email` VARCHAR(64) NOT NULL,
+  `nickname` VARCHAR(32) DEFAULT NULL,
+  `avator` varchar(64) DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
+  `created` VARCHAR(10) NOT NULL,
+  `lastlogin` VARCHAR(10) NOT NULL,
+  `status` SMALLINT(8) DEFAULT NULL,
+  UNIQUE KEY (`name`),
+  UNIQUE KEY (`nickname`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+-- --------------------------------------------------------
 
 --
--- 插入之前先把表清空（truncate） `here_users`
+-- Table structure for table `here_options`
 --
 
-TRUNCATE TABLE `h_users`;
+CREATE TABLE IF NOT EXISTS `here_options` (
+  `name` VARCHAR(16) NOT NULL,
+  `value` TEXT NOT NULL,
+  `for` SMALLINT(8) DEFAULT 0,
+  UNIQUE `option` (`name`, `for`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `here_posts`
+--
+
+CREATE TABLE IF NOT EXISTS `here_posts` (
+  `pid` SMALLINT(8) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` VARCHAR(64) NOT NULL,
+  `description` TEXT DEFAULT NULL,
+  `author_id` SMALLINT UNSIGNED NOT NULL,
+  `author` VARCHAR(32) NOT NULL,
+  `created` VARCHAR(10) NOT NULL,
+  `modified` VARCHAR(10) NOT NULL,
+  `content` TEXT DEFAULT NULL,
+  `type` VARCHAR(16) DEFAULT NULL,
+  `access` VARCHAR(16) DEFAULT 'public',
+  `password` VARCHAR(32) DEFAULT NULL,
+  `comment_cnt` SMALLINT(8) UNSIGNED NOT NULL DEFAULT 0,
+  `permission` SET('allowComment', 'allowFeed', 'allowCopy') NOT NULL,
+  `parent` SMALLINT(8) UNSIGNED NULL DEFAULT 0,
+  UNIQUE KEY (`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `here_classify`
+--
+
+CREATE TABLE IF NOT EXISTS `here_classify` (
+  `cid` SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `type` VARCHAR(16) NOT NULL DEFAULT 'comment',
+  `value` VARCHAR(64) NOT NULL,
+  `parent` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+  UNIQUE KEY `category` (`type`, `value`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `here_comments`
+--
+
+CREATE TABLE IF NOT EXISTS `here_comments` (
+  `cid` SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `pid` SMALLINT UNSIGNED NOT NULL,
+  `author` VARCHAR(32) NOT NULL,
+  `email` VARCHAR(64) DEFAULT NULL,
+  `url` VARCHAR(64) DEFAULT NULL,
+  `created` VARCHAR(10) NOT NULL,
+  `ip` VARCHAR(64) DEFAULT NULL,
+  `agent` VARCHAR(128) DEFAULT NULL,
+  `content` TEXT NOT NULL,
+  `approved` VARCHAR(8) NOT NULL DEFAULT 'wait',
+  `parent` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+  UNIQUE KEY `comment` (`created`, `ip`, `agent`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
