7574fbb693e27f45b4d3fb38925dc3a3874d7ec7
JShadowMan <jshadowman@163.com>
2016-07-08 18:35:50 +0800

    <fix>(Widget):fix a bug

--- include/Core/Here/Widget/Theme/Renderer/Footer.php
+++ include/Core/Here/Widget/Theme/Renderer/Footer.php
@@ -5,17 +5,25 @@
  */
 
 class Widget_Theme_Renderer_Footer extends Abstract_Widget {
+    /**
+     * referer theme information
+     * 
+     * @var array
+     */
     private static $_theme = null;
 
-    public function __construct($file, $theme) {
+    public function __construct($file) {
         parent::__construct();
 
         try {
-            $this->_theme = $theme;
             $this->_config->file = $this->_findThemeFile($file);
         } catch (Exception $except) {
             throw $except;
         }
+
+        $this->_config->favicon = false;
+        $this->_config->stylesheet = array();
+        $this->_config->javascript = array();
 
         return $this;
     }
@@ -24,7 +32,6 @@ class Widget_Theme_Renderer_Footer extends Abstract_Widget {
         self::$_theme = $theme;
         self::$_theme['pluginResourceRoot'] = __HERE_PLUGINS_DIRECTORY__;
     }
-    
 
     public function render() {
         include $this->_config->file;
@@ -35,20 +42,19 @@ class Widget_Theme_Renderer_Footer extends Abstract_Widget {
     }
 
     private function _findThemeFile($file) {
-        $file = $this->_theme['root'] . '%s%' . '/'
+        $file = self::$_theme['root'] . '%s%' . '/'
                 . ((((strpos($file, '.php') + 4) == strlen($file)) ? $file : ($file . '.php')));
 
         # Include Customer Theme File
-        if (is_file(str_replace('%s%', $this->_theme['name'], $file))) {
-            return str_replace('%s%', $this->_theme['name'], $file);
+        if (is_file(str_replace('%s%', self::$_theme['name'], $file))) {
+            return str_replace('%s%', self::$_theme['name'], $file);
         # Include Default Theme File
-        } else if (is_file(str_replace('%s%', $this->_theme['default'], $file))) {
-            return str_replace('%s%', $this->_theme['default'], $file);
+        } else if (is_file(str_replace('%s%', self::$_theme['default'], $file))) {
+            return str_replace('%s%', self::$_theme['default'], $file);
         # File Not Found
         } else {
-            throw new Exception('Cannot include theme file' . $file, 1);
+            throw new Exception('Cannot include theme file ' . $file, 1);
         }
     }
-    
 }
 
