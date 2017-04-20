fb88f824af16d62f0ccda4c5eca9db1e557da98a
ShadowMan <shadowman@shellboot.com>
2017-04-20 18:33:00 +0800

    remove all css file, using compass

---
+++ bin/start_server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2016-2017 ShadowMan
+#
+import os
+import sys
+import logging
+from multiprocessing import Process
+
+
+def init_logger():
+    pass
+
+
+def execute_command():
+    pass
+
+
+def command_monitor():
+    pass
+
+
+if __name__ == '__main__':
+    # python installer_guide.py 1>2 2>/dev/null
+    pass
