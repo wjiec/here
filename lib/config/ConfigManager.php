ae0c94aba38c68bc5b3ee0952e1c7e6852b6bb85
JShadowMan <shadowman@shellboot.com>
2017-09-08 23:16:54 +0800

    Yaml and manager

---
+++ lib/config/ConfigManager.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * ConfigManager.php
+ *
+ * @package   Here
+ * @author    ShadowMan <shadowman@shellboot.com>
+ * @copyright Copyright (C) 2016-2017 ShadowMan
+ * @license   MIT License
+ * @link      https://github.com/JShadowMan/here
+ */
+namespace Here\Lib\Config;
+
+
+/**
+ * Class ConfigManager
+ * @package Here\Lib\Config
+ */
+class ConfigManager {
+    /**
+     * @param string $config_name
+     */
+    final public static function get_conf($config_name) {
+    }
+}
---
+++ lib/config/YamlConfig.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * YamlConfig.php
+ *
+ * @package   Here
+ * @author    ShadowMan <shadowman@shellboot.com>
+ * @copyright Copyright (C) 2016-2017 ShadowMan
+ * @license   MIT License
+ * @link      https://github.com/JShadowMan/here
+ */
+namespace Here\Lib\Config;
+
+
+/**
+ * Class YamlConfig
+ * @package Here\Lib\Config
+ */
+class YamlConfig {
+    /**
+     * @param string $string
+     */
+    final public static function load($string) {
+    }
+
+    /**
+     * @param string $file_path
+     */
+    final public static function load_file($file_path) {
+    }
+
+    /**
+     * @param array $object
+     * @param int $indent
+     */
+    final public static function dump(array $object, $indent = 2) {
+    }
+
+    /**
+     * @param array $object
+     * @param int $indent
+     */
+    final public static function dump_file(array $object, $indent = 2) {
+    }
+}
