752c5e4e022b78413878f58e02ddc3f87ce042bb
JShadowMan <shadowman@shellboot.com>
2017-10-17 22:49:47 +0800

    rename App

---
+++ App/Blogger/HereBlogger.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * HereBlogger.php
+ *
+ * @package   Here
+ * @author    ShadowMan <shadowman@shellboot.com>
+ * @copyright Copyright (C) 2016-2017 ShadowMan
+ * @license   MIT License
+ * @link      https://github.com/JShadowMan/here
+ */
+namespace Here\App\Blogger;
+
+
+/**
+ * Class HereBlogger
+ * @package Here\App\Blogger
+ */
+final class HereBlogger {
+}
--- index.php
+++ index.php
@@ -15,28 +15,9 @@ declare(strict_types=1);
 
 /* namespaces definitions */
 namespace Here;
-use Here\Config\UserCollector;
-use Here\Lib\Loader\Autoloader;
-use Here\Lib\Io\Input\Request;
-use Here\Lib\Router\Dispatcher;
 
 /* root absolute path with `Here` */
 define('__HERE_ROOT_DIRECTORY__', str_replace('\\', '/', dirname(__FILE__)));
 
 /* the only explicit `require_once` to include `Autoloader` */
-require_once 'lib/loader/Autoloader.php';
-
-/* register root directory (must be call first) */
-Autoloader::set_root('Here', __HERE_ROOT_DIRECTORY__);
-
-/* register `Here\Lib` namespace */
-Autoloader::register('Here\Lib', '/lib');
-
-/* register `Here\Conf` namespace */
-Autoloader::register('Here\Config', '/etc');
-
-/* create router dispatcher instance */
-$router_dispatcher = new Dispatcher(new UserCollector());
-
-/* start dispatch */
-$router_dispatcher->dispatch(Request::request_method(), Request::request_uri());
+require_once 'Lib/Loader/Autoloader.php';
