d95efa08d0645f3b105e50d5042158877774e0d7
JShadowMan <shadowman@shellboot.com>
2017-12-07 23:05:53 +0800

    new stack module and remove fsm

---
+++ Lib/Ext/Stack/StackGlimpseIndexError.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * StackGlimpseIndexError.php
+ *
+ * @package   Here
+ * @author    ShadowMan <shadowman@shellboot.com>
+ * @copyright Copyright (C) 2016-2017 ShadowMan
+ * @license   MIT License
+ * @link      https://github.com/JShadowMan/here
+ */
+namespace Here\Lib\Ext\Stack;
+use Here\Lib\Exceptions\ExceptionBase;
+use Here\Lib\Exceptions\Level\Warning;
+
+
+/**
+ * Class StackGlimpseIndexError
+ * @package Here\Lib\Ext\Stack
+ */
+final class StackGlimpseIndexError extends ExceptionBase {
+    use Warning;
+}
