b9c671c49781fcd393472b49944199c437a8366e
Jayson <shadowman@shellboot.com>
2019-10-29 22:19:45 +0800

    SetupWizard service

---
+++ app/provider/Wizard/ServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * here application
+ *
+ * @package   here
+ * @author    Jayson Wang <jayson@laboys.org>
+ * @copyright Copyright (C) 2016-2019 Jayson Wang
+ * @license   MIT License
+ * @link      https://github.com/nosjay/here
+ */
+namespace Here\Provider\Wizard;
+
+use Here\Provider\AbstractServiceProvider;
+
+
+/**
+ * Class ServiceProvider
+ * @package Here\Provider\Wizard
+ */
+final class ServiceProvider extends AbstractServiceProvider {
+
+    /**
+     * Name of the service
+     *
+     * @var string
+     */
+    protected $service_name = 'wizard';
+
+    /**
+     * @inheritdoc
+     */
+    final public function register() {
+        $this->di->setShared($this->service_name, function() {
+            return new SetupWizard();
+        });
+    }
+
+}
---
+++ app/provider/Wizard/SetupWizard.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * here application
+ *
+ * @package   here
+ * @author    Jayson Wang <jayson@laboys.org>
+ * @copyright Copyright (C) 2016-2019 Jayson Wang
+ * @license   MIT License
+ * @link      https://github.com/nosjay/here
+ */
+namespace Here\Provider\Wizard;
+
+use Here\Model\Author;
+use Phalcon\Cache\BackendInterface;
+
+
+/**
+ * Class SetupWizard
+ * @package Here\Provider\Wizard
+ */
+final class SetupWizard {
+
+    /**
+     * @var BackendInterface
+     */
+    protected $cache;
+
+    /**
+     * Name of the wizard redis cache
+     *
+     * @var string
+     */
+    protected $wizard_key = 'wizard';
+
+    /**
+     * Create SetupWizard
+     */
+    final public function __construct() {
+        $this->cache = container('cache');
+    }
+
+    /**
+     * Checks the application has initialized
+     *
+     * @return bool
+     */
+    final public function isInitialized(): bool {
+        if ($this->cache->exists($this->wizard_key)) {
+            return true;
+        }
+        return !!Author::findFirst();
+    }
+
+}
