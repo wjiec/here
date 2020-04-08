defc04ec18bcd50189b8bf8dc552fea25eec2c38
Jayson <shadowman@shellboot.com>
2020-04-08 23:20:30 +0800

    update configs

--- app/config/config.php
+++ app/config/config.php
@@ -7,11 +7,5 @@
  * @link      https://github.com/wjiec/here
  */
 return [
-    'blog' => [
-        'name'          => env('HERE_NAME'),
-        'url'           => env('HERE_URL'),
-        'keywords'      => env('HERE_KEYWORDS'),
-        'description'   => env('HERE_DESCRIPTION'),
-        'robots'        => env('HERE_ALLOW_SPIDER')
-    ]
+    'blog' => []
 ];
